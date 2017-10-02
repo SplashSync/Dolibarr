86a8a3e6ab6906bc8319aa2c7bcee249309ed921
BadPixxel <eshop.bpaquier@gmail.com>
2017-10-03 01:02:20 +0200

    Work on Travis Config Signed-off-by:BadPixxel <eshop.bpaquier@gmail.com>

--- build/install_database.sh
+++ build/install_database.sh
@@ -5,7 +5,17 @@ mysql -e 'GRANT ALL PRIVILEGES ON travis.* TO travis@127.0.0.1;'
 mysql -e 'FLUSH PRIVILEGES;'    
 
 echo "Loading demo Data"
-mysql -D travis < $DOL_BUILD_DIR/dev/initdemo/mysqldump_dolibarr_$DATA_VERSION.sql
+
+FILE=$DOL_BUILD_DIR/dev/initdemo/mysqldump_dolibarr_$DATA_VERSION.sql    
+if [ -f $FILE ]; then
+   mysql -D travis < $DOL_BUILD_DIR/dev/initdemo/mysqldump_dolibarr_$DATA_VERSION.sql
+else
+    FILE2=$DOL_BUILD_DIR/dev/initdata/mysqldump_dolibarr_$DATA_VERSION.sql    
+    if [ -f $FILE2 ]; then
+        mysql -D travis < $DOL_BUILD_DIR/dev/initdata/mysqldump_dolibarr_$DATA_VERSION.sql
+    fi
+fi
+
 
 VERSION=`expr substr $DOL_VERSION 1 1`
 if [ "$VERSION" = "5" ];  
