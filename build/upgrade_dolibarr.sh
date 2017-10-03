7fe5c43b7340558d71e73ce3d1541add6d262772
BadPixxel <eshop.bpaquier@gmail.com>
2017-10-03 16:54:47 +0200

    Release of Dolibarr Module V1.1.0 Signed-off-by:BadPixxel <eshop.bpaquier@gmail.com>

--- build/upgrade_dolibarr.sh
+++ build/upgrade_dolibarr.sh
@@ -6,11 +6,9 @@ cd $DOL_BUILD_DIR/htdocs/install
 if [[ $DOL_VERSION == "develop" ]]; 
 then 
     echo "Upgrading for Dolibarr Develop"
-    php upgrade.php 6.0.0 7.0.0 ignoredbversion > $TRAVIS_BUILD_DIR/upgrade600700.log
-    php upgrade2.php 6.0.0 7.0.0 MAIN_MODULE_API,MAIN_MODULE_SUPPLIERPROPOSAL > $TRAVIS_BUILD_DIR/upgrade600700-2.log
-    php step5.php 6.0.0 7.0.0 > $TRAVIS_BUILD_DIR/upgrade600700-3.log's
+    php upgrade.php 6.0.0 7.0.0 ignoredbversion                                 > $TRAVIS_BUILD_DIR/upgrade600700.log
+    php upgrade2.php 6.0.0 7.0.0 MAIN_MODULE_API,MAIN_MODULE_SUPPLIERPROPOSAL   > $TRAVIS_BUILD_DIR/upgrade600700-2.log
+    php step5.php 6.0.0 7.0.0                                                   > $TRAVIS_BUILD_DIR/upgrade600700-3.log
 fi  
 
 echo
-
-
