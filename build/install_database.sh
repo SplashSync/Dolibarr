1910b73a5e93956d608f7f85fb4f5fa42a8ccfc8
BadPixxel <eshop.bpaquier@gmail.com>
2018-08-29 19:52:03 +0200

    WIP : PhpUnit

--- build/install_database.sh
+++ build/install_database.sh
@@ -32,8 +32,8 @@ fi
 
 if [ "$VERSION" = "8" ];  
 then 
-    echo "BugFix Update for Dolibarr 4.0.0"
-    mysql -D travis -e "ALTER TABLE llx_website_page DROP COLUMN fk_user_creat;"
+    echo "BugFix Update for Dolibarr 8.0.0"
+    mysql -D travis -e "ALTER TABLE llx_website_page DROP COLUMN fk_user_create;"
     mysql -D travis -e "ALTER TABLE llx_website_page DROP COLUMN fk_user_modif;"
     mysql -D travis -e "ALTER TABLE llx_website_page DROP COLUMN type_container;"
 
