6433c61363ae201a771a8a3b21c4ad568da422c0
BadPixxel <eshop.bpaquier@gmail.com>
2019-02-05 01:20:19 +0100

    WIP: Upgrade to Splash V2

---
+++ autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2019 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/splash/vendor/autoload.php';
