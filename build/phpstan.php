fedb19a91c67529c78e7abad8e383dc0d3e9da69
BadPixxel <eshop.bpaquier@gmail.com>
2019-03-17 21:16:59 +0100

    PhpStan Only: Fix Global Conf Variables

--- build/phpstan.php
+++ build/phpstan.php
@@ -1,15 +1,30 @@
 <?php
-/**
- * Bootstrap Dolibarr for Pḧpstan
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2019 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
  */
 
-require dirname(__DIR__) . "/splash/vendor/autoload.php";
+require dirname(__DIR__)."/splash/vendor/autoload.php";
 
 global $db,$langs,$conf,$user,$hookmanager;
 
 //====================================================================//
+// Initiate Some Dolibarr Global Configs
+$conf->global = new stdClass();
+$conf->facture = new stdClass();
+
+//====================================================================//
 // Initiate Dolibarr Global Envirement Variables
-require_once(dirname(dirname(__DIR__)) . "/master.inc.php");
+require_once(dirname(dirname(__DIR__))."/master.inc.php");
 
 //====================================================================//
 // Functions Dolibarr
@@ -18,7 +33,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/ecm/class/ecmfiles.class.php';
 require_once(DOL_DOCUMENT_ROOT.'/core/lib/product.lib.php');
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.form.class.php';
-require_once DOL_DOCUMENT_ROOT . '/core/class/ccountry.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/ccountry.class.php';
 
 //====================================================================//
 // Objects Classes
@@ -29,12 +44,12 @@ require_once DOL_DOCUMENT_ROOT.'/compta/paiement/class/paiement.class.php';
 require_once DOL_DOCUMENT_ROOT.'/compta/paiement/class/cpaiement.class.php';
 require_once DOL_DOCUMENT_ROOT.'/compta/bank/class/account.class.php';
 require_once DOL_DOCUMENT_ROOT.'/product/stock/class/mouvementstock.class.php';
-        
+
 require_once DOL_DOCUMENT_ROOT."/variants/class/ProductAttribute.class.php";
 require_once DOL_DOCUMENT_ROOT."/variants/class/ProductAttributeValue.class.php";
 require_once DOL_DOCUMENT_ROOT."/variants/class/ProductCombination.class.php";
 require_once DOL_DOCUMENT_ROOT."/variants/class/ProductCombination2ValuePair.class.php";
-        
+
 //====================================================================//
 // Widgets Classes
 require_once(DOL_DOCUMENT_ROOT.'/compta/facture/class/facturestats.class.php');
@@ -50,11 +65,11 @@ include_once DOL_DOCUMENT_ROOT.'/expensereport/class/expensereport.class.php';
 
 //====================================================================//
 // Splash Dolibarr Specific Classes
-include_once dirname(__DIR__) . '/splash/core/modules/modSplash.class.php';
-include_once dirname(__DIR__) . '/splash/core/triggers/interface_50_modSplash_Splash.class.php';
+include_once dirname(__DIR__).'/splash/core/modules/modSplash.class.php';
+include_once dirname(__DIR__).'/splash/core/triggers/interface_50_modSplash_Splash.class.php';
 
 //====================================================================//
 // Include Splash Constants Definitions
-require_once(dirname(dirname(__FILE__)) . "/splash/vendor/splash/phpcore/inc/Splash.Inc.php");
-require_once(dirname(dirname(__FILE__)) . "/splash/vendor/splash/phpcore/inc/defines.inc.php");
+require_once(dirname(dirname(__FILE__))."/splash/vendor/splash/phpcore/inc/Splash.Inc.php");
+require_once(dirname(dirname(__FILE__))."/splash/vendor/splash/phpcore/inc/defines.inc.php");
 define("SPLASH_SERVER_MODE", 0);
