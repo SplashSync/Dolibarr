f67a1bc3a493915a62a0acfde7bf0ecdfbe27341
BadPixxel <eshop.bpaquier@gmail.com>
2019-06-27 00:46:44 +0200

    Imporve Tax Names Detection

--- splash/src/Core/BaseItemsTrait.php
+++ splash/src/Core/BaseItemsTrait.php
@@ -412,7 +412,7 @@ trait BaseItemsTrait
         }
         //====================================================================//
         // Clean VAT Code
-        $taxName = preg_replace('/\s/', '', $itemData["vat_src_code"]);
+        $taxName = preg_replace('/%/', '', preg_replace('/\s/', '', $itemData["vat_src_code"]));
         $cleanedTaxName = is_string($taxName) ? substr($taxName, 0, 10) : "0";
         //====================================================================//
         // Update VAT Code if Needed
@@ -437,7 +437,6 @@ trait BaseItemsTrait
         if (!$identifiedVat) {
             return;
         }
-
         //====================================================================//
         // Update Rates from Vat Type
         $this->currentItem->tva_tx = $identifiedVat->tva_tx;
