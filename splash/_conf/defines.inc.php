4952fc7740ec5ba0a55d85e09e92f527eba3ac07
BadPixxel <eshop.bpaquier@gmail.com>
2021-02-06 12:04:43 +0100

    UPDATE HEADER 2021

--- splash/_conf/defines.inc.php
+++ splash/_conf/defines.inc.php
@@ -3,7 +3,7 @@
 /*
  *  This file is part of SplashSync Project.
  *
- *  Copyright (C) 2015-2020 Splash Sync  <www.splashsync.com>
+ *  Copyright (C) 2015-2021 Splash Sync  <www.splashsync.com>
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -32,7 +32,7 @@ define("SPL_SHOW_DEV", 0);
 
 //====================================================================//
 // Module Version
-define("SPL_MOD_VERSION", '1.8.1');
+define("SPL_MOD_VERSION", '1.8.2');
 define("SPL_MOD_ID", 9200);
 define("SPL_MOD_NAME", 'splash');
 define("SPL_MOD_CATEGORIE", 'technic');
