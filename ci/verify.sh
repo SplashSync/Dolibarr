f4c3a1c7f5f4a47557ac6f1e901aafa4a226d894
BadPixxel <eshop.bpaquier@gmail.com>
2022-05-24 11:19:03 +0200

    ADD: CI Verify Script

---
+++ ci/verify.sh
@@ -0,0 +1,48 @@
+
+################################################################################
+#
+#  This file is part of SplashSync Project.
+#
+#  Copyright (C) Splash Sync <www.splashsync.com>
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  For the full copyright and license information, please view the LICENSE
+#  file that was distributed with this source code.
+#
+#  @author Bernard Paquier <contact@splashsync.com>
+#
+################################################################################
+
+set -e
+
+################################################################################
+# Docker Compose Container you want to check
+CONTAINERS="dolibarr,dol-14"
+WORKDIR="/var/www/html/custom"
+################################################################################
+# Start Docker Compose Stack
+echo '===> Start Docker Stack'
+docker-compose up -d
+
+######################################
+# Run Grumphp Test Suites Locally
+php splash/vendor/bin/grumphp run --testsuite=travis
+php splash/vendor/bin/grumphp run --testsuite=csfixer
+
+######################################
+# Walk on Docker Compose Container
+for ID in $(echo $CONTAINERS | tr "," "\n")
+do
+    echo "----------------------------------------------------"
+    echo "===> CHECKS ON $ID"
+    echo "----------------------------------------------------"
+    # Ensure Git is Installed
+    docker-compose exec $ID apt update
+    docker-compose exec $ID apt install git -y
+    # Run Grumphp Test Suites
+    docker-compose exec -w $WORKDIR $ID php splash/vendor/bin/grumphp run --testsuite=travis
+    docker-compose exec -w $WORKDIR $ID php splash/vendor/bin/grumphp run --testsuite=phpstan
+done
