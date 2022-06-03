1405b53ae8317a33847e7a57e9f39dda0721ec56
BadPixxel <eshop.bpaquier@gmail.com>
2022-06-03 14:25:41 +0200

    Release of V1.15.2

--- splash/_conf/defines.inc.php
+++ splash/_conf/defines.inc.php
@@ -32,7 +32,7 @@ define("SPL_SHOW_DEV", 0);
 
 //====================================================================//
 // Module Version
-define("SPL_MOD_VERSION", '1.15.2-RC1');
+define("SPL_MOD_VERSION", '1.15.2');
 define("SPL_MOD_ID", 9200);
 define("SPL_MOD_NAME", 'splash');
 define("SPL_MOD_CATEGORIE", 'technic');
